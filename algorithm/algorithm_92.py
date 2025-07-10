2e3e86c4303d64170198544c21c4c9e911affd72
kungminno <kungminno@naver.com>
2025-07-10 11:38:31 +0900

    algorithm_250710

--- algorithm/algorithm_92.py
+++ algorithm/algorithm_92.py
@@ -1,1 +1,19 @@
-def solution()
+from collections import deque
+def solution(priorities, location):
+    process = [(i,v) for i,v in enumerate(priorities)]
+    process = deque(process)
+    priorities.sort()
+    max_v = priorities[-1]
+    
+    count = 0
+    while True:
+        turn, pri = process.popleft()
+
+        if pri == max_v:
+            count+=1
+            if turn == location:
+                return count
+            priorities.pop()
+            max_v = priorities[-1]
+        else:
+            process.append((turn, pri))
